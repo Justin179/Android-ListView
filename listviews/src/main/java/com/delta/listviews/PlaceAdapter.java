7b1ff4af66ee3a9eab7b03a01f13714fb512cf62
Justin179 <Justin179>
2017-02-10 11:51:03 +0800

    ListView sommth

--- listviews/src/main/java/com/delta/listviews/PlaceAdapter.java
+++ listviews/src/main/java/com/delta/listviews/PlaceAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -39,28 +40,53 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
     public View getView(int position, View convertView, ViewGroup parent) {
         //return super.getView(position, convertView, parent);
         View row = convertView;
-        // inflate the layout for a single row
-        LayoutInflater inflater = LayoutInflater.from(this.mContext);
-        row = inflater.inflate(mLayoutResourceId,parent,false);
+        PlaceHolder holder = null;
 
-        // get a reference to te different view elements we wish to update
-        TextView nameView = (TextView) row.findViewById(R.id.nameTextView);
-        TextView zipView = (TextView) row.findViewById(R.id.zipcodeTextView);
-        ImageView imageView = (ImageView)  row.findViewById(R.id.imageView);
+        if(row==null){
+            // inflate the layout for a single row
+            LayoutInflater inflater = LayoutInflater.from(this.mContext);
+            row = inflater.inflate(mLayoutResourceId,parent,false);
 
-        // get the data from data Array
+            holder = new PlaceHolder();
+            // get a reference to te different view elements we wish to update
+            holder.nameView = (TextView) row.findViewById(R.id.nameTextView);
+            holder.zipView = (TextView) row.findViewById(R.id.zipcodeTextView);
+            holder.imageView = (ImageView)  row.findViewById(R.id.imageView);
+
+            // the magic line
+            row.setTag(holder);
+        } else {
+            // otherwise use an existing one (直接用既有的，速度就差在這裡)
+            holder = (PlaceHolder) row.getTag();
+        }
+
+        // getting the data from data array
         Place place = mData[position];
 
-        // setting the view to reflect the data
-        nameView.setText(place.mNameOfPlace);
-        zipView.setText(String.valueOf(place.mZipCode));
+        // setup and reuse the same listener for each row
+        holder.imageView.setOnClickListener(PopupListener);
+        Integer rowPosition = position;
+        holder.imageView.setTag(rowPosition);
 
-        int resId = mContext.getResources().getIdentifier(place.mNameOfImage,"drawable",mContext.getPackageName());
-        imageView.setImageResource(resId);
+        // setting the view to reflect the data we need to display
+        holder.nameView.setText(place.mNameOfPlace);
+        holder.zipView.setText(String.valueOf(place.mZipCode));
+        // for getting the image
+        int resID = mContext.getResources().getIdentifier(place.mNameOfImage,"drawable", mContext.getPackageName());
+        holder.imageView.setImageResource(resID);
 
         //
         return row;
     }
+
+    View.OnClickListener PopupListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Integer viewPosition = (Integer) v.getTag();
+            Place p = mData[viewPosition];
+            Toast.makeText(getContext(),p.mPopup,Toast.LENGTH_SHORT).show();
+        }
+    };
 
     private static class PlaceHolder {
         TextView nameView;
